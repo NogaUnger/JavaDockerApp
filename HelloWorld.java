902a52106ac5a40911ce911ca5f704cc63dcf463
Noga Unger <nogi.unger@gmail.com>
2025-05-03 18:55:41 +0300

    "Second"

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,8 @@ package JavaDockerApp;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Java + Docker + GitHub!");
+        System.out.println("second");
     }
+
+
 }
